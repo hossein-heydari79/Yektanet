48a4459634fad3b65ef0018d1e1a1d40862e14a0
hossein-heydari <hosseinheydari790228@gmail.com>
2021-08-07 20:26:14 +0430

    add header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hello yektanet
+
+      <div className="header">
+        <div className="inputs">
+          <label htmlFor="label-filed">فیلد</label>
+          <input type="text" id="label-filed" />
+        </div>
+
+        <div className="inputs">
+          <label htmlFor="label-ads">نام آگهی</label>
+          <input type="text" id="label-ads" />
+        </div>
+
+        <div className="inputs">
+          <label htmlFor="label-date">تاریخ</label>
+          <input type="text" id="label-date" />
+        </div>
+
+        <div className="inputs">
+          <label htmlFor="label-change">نام تغییر دهنده</label>
+          <input type="text" id="label-change" />
+        </div>
+      </div>
+
     </div>
   );
 }
