018cd73e811d134bdb6dd286d2bb33b7e9afb4ce
hossein-heydari <hosseinheydari790228@gmail.com>
2021-08-08 19:01:21 +0430

    fix filter items

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -82,22 +82,22 @@ export const Header = () => {
         <div className={styles.header}>
             <div className={styles.filter_item}>
                 <label htmlFor="label-filed">فیلد</label>
-                <input type="text" id="label-filed" name="field" onChange={(e) => { dispatch(addInputsValue({ ...inputsValue, [e.target.name]: e.target.value })); addQuery(e.target.name, e.target.value) }} />
+                <input type="text" id="label-filed" value={inputsValue.field} name="field" onChange={(e) => { dispatch(addInputsValue({ ...inputsValue, [e.target.name]: e.target.value })); addQuery(e.target.name, e.target.value) }} />
             </div>
 
             <div className={styles.filter_item}>
                 <label htmlFor="label-ads">نام آگهی</label>
-                <input type="text" id="label-ads" name="ads" onChange={(e) => { dispatch(addInputsValue({ ...inputsValue, [e.target.name]: e.target.value })); addQuery(e.target.name, e.target.value) }} />
+                <input type="text" id="label-ads" value={inputsValue.ads} name="ads" onChange={(e) => { dispatch(addInputsValue({ ...inputsValue, [e.target.name]: e.target.value })); addQuery(e.target.name, e.target.value) }} />
             </div>
 
             <div className={styles.filter_item}>
                 <label htmlFor="label-date">تاریخ</label>
-                <input type="text" name="date" id="label-date" onChange={(e) => { dispatch(addInputsValue({ ...inputsValue, [e.target.name]: e.target.value })); addQuery(e.target.name, e.target.value) }} />
+                <input type="text" value={inputsValue.date} name="date" id="label-date" onChange={(e) => { dispatch(addInputsValue({ ...inputsValue, [e.target.name]: e.target.value })); addQuery(e.target.name, e.target.value) }} />
             </div>
 
             <div className={styles.filter_item}>
                 <label htmlFor="label-change">نام تغییر دهنده</label>
-                <input type="text" name="nameChanger" id="label-change" onChange={(e) => {
+                <input type="text" value={inputsValue.nameChanger} name="nameChanger" id="label-change" onChange={(e) => {
                     dispatch(addInputsValue({ ...inputsValue, [e.target.name]: e.target.value }));
                     addQuery(e.target.name, e.target.value)
                 }} />
